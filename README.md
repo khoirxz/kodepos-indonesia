fab28eb8bb87992f17a1e8f5b9a707bdc8d1f198
khoirxz <khoirrurrohman@gmail.com>
2024-01-31 12:06:50 +0700

    add route

---
+++ README.md
@@ -0,0 +1,23 @@
+# API KODEPOS INDONESIA
+
+example usage
+`get http://localhost:5000/?search=jakarta`
+
+return
+`[
+  {
+    "province": "DKI Jakarta",
+    "city": "Jakarta Barat",
+    "district": "Kalideres",
+    "subdistrict": "Semanan",
+    "postal_code": "11850"
+  },
+  {
+    "province": "DKI Jakarta",
+    "city": "Jakarta Barat",
+    "district": "Kalideres",
+    "subdistrict": "Tegal Alur",
+    "postal_code": "11820"
+  },
+  ...
+]`
---
+++ index.js
@@ -0,0 +1,48 @@
+import express from "express";
+import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path"; // Import join from path module
+import fs from "fs/promises";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", async (req, res) => {
+  const searchCriteria = req.query.search;
+
+  if (!searchCriteria) {
+    res.status(200).send("API KODEPOS INDONESIA.");
+    return;
+  }
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const filePath = join(__dirname, "kodepos.json"); // Use join from path module
+
+  try {
+    const rawData = await fs.readFile(filePath, "utf8");
+    const data = JSON.parse(rawData);
+
+    const filteredData = data.filter((item) => {
+      const lowercasedSearch = searchCriteria.toLowerCase();
+      return (
+        item.province.toLowerCase().includes(lowercasedSearch) ||
+        item.city.toLowerCase().includes(lowercasedSearch) ||
+        item.district.toLowerCase().includes(lowercasedSearch) ||
+        item.subdistrict.toLowerCase().includes(lowercasedSearch) ||
+        item.postal_code.toLowerCase().includes(lowercasedSearch)
+      );
+    });
+
+    res.send(filteredData);
+  } catch (err) {
+    console.error("Error reading or parsing JSON file:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server running on http://localhost:${process.env.PORT || 5000}`);
+});
